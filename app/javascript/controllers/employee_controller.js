7c10b1a5fef593264a267085015632aa80d394c2
Fedor Kosolapov <fedor_coder@mail.ru>
2021-03-17 12:47:55 +0700

    add creating new employee

--- app/javascript/controllers/employee_controller.js
+++ app/javascript/controllers/employee_controller.js
@@ -1,14 +1,20 @@
 import ApplicationController from './application_controller.js'
 
 export default class extends ApplicationController {
-    delete(event) {
-      event.preventDefault()
+  delete(event) {
+    event.preventDefault()
 
-      const empl_row_id = event.target.dataset.id
-      document.getElementById(empl_row_id).remove()
+    const empl_row_id = event.target.dataset.id
+    document.getElementById(empl_row_id).remove()
 
-      const empl_id = parseInt(empl_row_id.split('_')[1])
-      console.log(empl_id)
-      this.stimulate('Employee#delete', empl_id)
+    const empl_id = parseInt(empl_row_id.split('_')[1])
+    console.log(empl_id)
+    this.stimulate('Employee#delete', empl_id)
+  }
+  add(event) {
+    event.preventDefault()
+    const new_name = document.getElementById('new_name').value
+    const new_last_name = document.getElementById('new_last_name').value
+    this.stimulate('Employee#add', new_name, new_last_name)
   }
 }
--- app/reflexes/employee_reflex.rb
+++ app/reflexes/employee_reflex.rb
@@ -2,4 +2,8 @@ class EmployeeReflex < ApplicationReflex
   def delete id
     Employee.find(id).delete
   end
+
+  def add name, last_name
+    Employee.new(name: name, last_name: last_name).save
+  end
 end
